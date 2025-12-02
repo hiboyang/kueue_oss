44cd953bc48d41b3a4895b35abe876f303663a9f
Bo (AIML) Yang <bo_yang6@apple.com>
2025-12-02 11:44:29 -0800

    Reenable autoscaling

--- pkg/util/testingjobs/rayjob/wrappers.go
+++ pkg/util/testingjobs/rayjob/wrappers.go
@@ -308,7 +308,7 @@ func (j *JobWrapper) AddAnnotation(key string, value string) *JobWrapper {
 func (j *JobWrapper) EnableInTreeAutoscaling() *JobWrapper {
 	enable := true
 	aggressive := rayv1.UpscalingMode("Aggressive")
-	idleTimeoutSeconds := int32(10)
+	idleTimeoutSeconds := int32(30)
 	j.Spec.RayClusterSpec.EnableInTreeAutoscaling = &enable
 	j.Spec.RayClusterSpec.AutoscalerOptions = &rayv1.AutoscalerOptions{
 		UpscalingMode:      &aggressive,
